d6346693dfab19c97e414b7d8878373b75e098d9
brightsdekim2 <samsungyounkim@gmail.com>
2025-12-16 13:03:03 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import CourtListScreen from './CourtListScreen';
 import CourtDetailScreen from './CourtDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [reviews, setReviews] = useState([]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Courts"
-          component={CourtListScreen}
-        />
-        <Stack.Screen
-          name="Court"
-          component={CourtDetailScreen}
-        />
+        <Stack.Screen name="CourtList" component={CourtListScreen} />
+        <Stack.Screen name="CourtDetail">
+          {props => (
+            <CourtDetailScreen
+              {...props}
+              reviews={reviews}
+              setReviews={setReviews}
+            />
+          )}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
